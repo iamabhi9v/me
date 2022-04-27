a70b7261aecac3d9965bfb02a39fc0d65a679243
Abhinav Anand <abhi9vanand@gmail.com>
2022-04-27 21:40:40 +0530

    index js edited

--- pages/index.js
+++ pages/index.js
@@ -40,7 +40,13 @@ const Home = () => (
 
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
-          <Heading as="h2" variant="page-title">
+          <Heading as="h2"
+           variant="page-title"
+           bgColor="blue.400"
+           bgClip="text"
+           bgGradient="linear(to-l, #79c2ff, #4a5888)"
+           //className="jumbotron-name-heading"
+           >
             Abhinav Anand
           </Heading>
           <p>Software Developer / Data Analyst / Actor & Film-maker / Photographer / Video Gamer</p>
